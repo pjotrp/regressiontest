ed86d6c542a72527fff393ee8f4681d1802d5481
Pjotr Prins <pjotr.public01@thebird.nl>
2012-08-20 11:21:12 +0200

    Add filter to output

--- lib/regressiontest/cli_exec.rb
+++ lib/regressiontest/cli_exec.rb
@@ -1,9 +1,14 @@
+require 'fileutils'
+
 module RegressionTest
 
   DEFAULT_TESTDIR = "test/data/regression"
 
   # Regression test runner compares output in ./test/data/regression (by default).
   # The convention is to have a file with names .ref (reference) and create .new
+  #
+  # You can add an :ignore regex option which ignores lines in the comparsion files 
+  # matching a regex
   module CliExec
     def CliExec::exec command, testname, options = {}
       # ---- Find .ref file
@@ -25,13 +30,27 @@ module RegressionTest
         $stderr.print cmd," returned an error\n"
         return false 
       end
+      if options[:ignore]
+        regex = options[:ignore]
+        outfn1 = outfn + ".1"
+        FileUtils.mv(outfn,outfn1)
+        buf = []
+        f1 = File.open(outfn1)
+        f = File.open(outfn,"w")
+        f1.each_line do | line |
+          f.print(line) if line !~ /#{regex}/
+        end
+        f1.close
+        f.close
+        FileUtils::rm(outfn1)
+      end
       # ---- Compare the two files
-      compare_files(outfn,reffn)
+      compare_files(outfn,reffn,options[:ignore])
     end
 
-    def CliExec::compare_files fn1, fn2
+    def CliExec::compare_files fn1, fn2, ignore = nil
       if not File.exist?(fn2)
-        Kernel.system("cp -v #{fn1} #{fn2}")
+        FileUtils::cp(fn1,fn2)
         true
       else
         cmd = "diff #{fn2} #{fn1}"
@@ -39,6 +58,7 @@ module RegressionTest
         return true if Kernel.system(cmd) == true
         $stderr.print "If it is correct, execute \"cp #{fn1} #{fn2}\", and run again"
         false
+      end
     end
   end
 
